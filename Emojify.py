73116a4db0678aea62f3cbc67d584d32ed433dd9
Wenyu Yang <vincentyoungmsu@gmail.com>
2020-03-18 14:13:14 +0800

    Add files via upload

---
+++ Emojify.py
@@ -0,0 +1,118 @@
+import numpy as np
+from keras.models import Model
+from keras.layers import Dense, Input, Dropout, LSTM, Activation, Embedding
+
+def sentences_to_indices(X, word_to_index, max_len):
+    """
+    Converts an array of sentences (strings) into an array of indices corresponding to words in the sentences.
+    The output shape should be such that it can be given to `Embedding()`. 
+    
+    Arguments:
+    X -- array of sentences (strings), of shape (m, 1)
+    word_to_index -- a dictionary containing the each word mapped to its index
+    max_len -- maximum number of words in a sentence. You can assume every sentence in X is no longer than this. 
+    
+    Returns:
+    X_indices -- array of indices corresponding to words in the sentences from X, of shape (m, max_len)
+    """
+    
+    # number of training examples
+    m = X.shape[0]                                   
+    
+    # Initialize X_indices as a numpy matrix of zeros and the correct shape (≈ 1 line)
+    X_indices = np.zeros((m, max_len))
+    
+    # loop over training examples
+    for i in range(m):                               
+        
+        # Convert the ith training sentence in lower case and split is into words. You should get a list of words.
+        sentence_words = X[i].lower().split()
+        
+        # Initialize j to 0
+        j = 0
+        
+        # Loop over the words of sentence_words
+        for w in sentence_words:
+            # Set the (i,j)th entry of X_indices to the index of the correct word.
+            X_indices[i, j] = word_to_index[w]
+            # Increment j to j + 1
+            j = j + 1
+            
+    return X_indices
+
+def pretrained_embedding_layer(word_to_vec_map, word_to_index):
+    """
+    Creates a Keras Embedding() layer and loads in pre-trained GloVe 50-dimensional vectors.
+    
+    Arguments:
+    word_to_vec_map -- dictionary mapping words to their GloVe vector representation.
+    word_to_index -- dictionary mapping from words to their indices in the vocabulary (400,001 words)
+
+    Returns:
+    embedding_layer -- pretrained layer Keras instance
+    """
+    
+    # adding 1 to fit Keras embedding
+    vocab_len = len(word_to_index) + 1   
+    # define dimensionality of the GloVe word vectors              
+    emb_dim = word_to_vec_map["cucumber"].shape[0]      
+    
+    # Initialize the embedding matrix as a numpy array of zeros.
+    emb_matrix = np.zeros((vocab_len, emb_dim))
+    
+    # Set each row "idx" of the embedding matrix to be 
+    # the word vector representation of the idx'th word of the vocabulary
+    for word, idx in word_to_index.items():
+        emb_matrix[idx, :] = word_to_vec_map[word]
+
+    # Define Keras embedding layer with the correct input and output sizes
+    # Make it non-trainable.
+    embedding_layer = Embedding(vocab_len, emb_dim, trainable=False)
+
+    # Build the embedding layer, it is required before setting the weights of the embedding layer. 
+    embedding_layer.build((None,))
+    
+    # Set the weights of the embedding layer to the embedding matrix.
+    embedding_layer.set_weights([emb_matrix])
+    
+    return embedding_layer
+
+def Emojify(input_shape, word_to_vec_map, word_to_index):
+    """
+    Function creating the Emojify model's graph.
+    
+    Arguments:
+    input_shape -- shape of the input, usually (max_len,)
+    word_to_vec_map -- dictionary mapping every word in a vocabulary into its 50-dimensional vector representation
+    word_to_index -- dictionary mapping from words to their indices in the vocabulary (400,001 words)
+
+    Returns:
+    model -- a model instance in Keras
+    """
+    
+    # Define sentence_indices as the input of the graph.
+    sentence_indices = Input(input_shape, dtype='int32')
+    
+    # Create the embedding layer pretrained with GloVe Vectors
+    embedding_layer = pretrained_embedding_layer(word_to_vec_map, word_to_index)
+    
+    # Propagate sentence_indices through your embedding layer
+    embeddings = embedding_layer(sentence_indices)   
+    
+    # Propagate the embeddings through an LSTM layer with 128-dimensional hidden state
+    X = LSTM(128, return_sequences=True)(embeddings)
+    # Add dropout with a probability of 0.5
+    X = Dropout(0.5)(X)
+    # Propagate X trough another LSTM layer with 128-dimensional hidden state
+    X = LSTM(128, return_sequences=False)(X)
+    # Add dropout with a probability of 0.5
+    X = Dropout(0.5)(X)
+    # Propagate X through a Dense layer with 5 units
+    X = Dense(5)(X)
+    # Add a softmax activation
+    X = Activation('softmax')(X)
+    
+    # Create Model instance which converts sentence_indices into X.
+    model = Model(inputs=sentence_indices, outputs=X)
+    
+    return model
